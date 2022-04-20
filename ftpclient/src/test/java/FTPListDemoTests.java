b116802bc7ebb29c3f142d34a0fbad60bf1bd11b
enextus <enextus@gmail.com>
2022-04-20 13:25:06 +0200

    20.04.2022

---
+++ ftpclient/src/test/java/FTPListDemoTests.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FTPListDemoTests {
+
+	@BeforeEach
+	void setUp() {
+	}
+
+	@AfterEach
+	void tearDown() {
+	}
+
+	@Test
+	void main() {
+		assertTrue(true);
+	}
+}
